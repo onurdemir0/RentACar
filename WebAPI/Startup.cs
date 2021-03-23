a046dbdece37dee78680870d5e936f90042c9422
Onur Demir <Onur.d77@outlook.com>
2021-03-24 00:09:04 +0300

    Granted access to API from frontend

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -50,6 +50,8 @@ namespace WebAPI
 			//services.AddSingleton<IUserService, UserManager>();
 			//services.AddSingleton<IUserDal, EfUserDal>();
 
+			services.AddCors();
+
 			var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
 			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -81,6 +83,8 @@ namespace WebAPI
 			}
 
 			app.ConfigureCustomExceptionMiddleware();
+
+			app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
 			app.UseHttpsRedirection();
 
